f48ce21f50360c7eadefa2780cd6f7878452493b
nkitlabs <natthakunk0109@gmail.com>
2025-02-04 21:30:33 +0700

    fix geth keystore

--- relayer/wallet/geth_keystore.go
+++ relayer/wallet/geth_keystore.go
@@ -3,7 +3,6 @@ package wallet
 import (
 	"crypto/ecdsa"
 	"fmt"
-	"os"
 	"path"
 
 	"github.com/ethereum/go-ethereum/accounts"
@@ -16,79 +15,52 @@ import (
 var _ Wallet = &GethKeyStoreWallet{}
 
 type GethKeyStoreWallet struct {
-	passphrase          string
-	store               *keystore.KeyStore
-	keyNameToHexAddress map[string]string
-	keyNamePath         string
+	Passphrase  string
+	Store       *keystore.KeyStore
+	KeyNameInfo map[string]string
+	HomePath    string
+	ChainName   string
 }
 
 // NewGethKeyStoreWallet creates a new GethKeyStoreWallet instance
-func NewGethKeyStoreWallet(passphrase, homeDir, chainName string) (*GethKeyStoreWallet, error) {
-	// create folders if not exists
-	if err := internal.CheckAndCreateFolder(homeDir); err != nil {
-		return nil, err
-	}
-
-	keyDir := path.Join(homeDir, "keys")
-	if err := internal.CheckAndCreateFolder(keyDir); err != nil {
-		return nil, err
-	}
-
-	keyChainDir := path.Join(keyDir, chainName)
-	if err := internal.CheckAndCreateFolder(keyChainDir); err != nil {
-		return nil, err
-	}
-
-	keyStoreDir := path.Join(keyChainDir, "priv")
-	if err := internal.CheckAndCreateFolder(keyStoreDir); err != nil {
-		return nil, err
-	}
-
-	keyNameDir := path.Join(keyChainDir, "info")
-	if err := internal.CheckAndCreateFolder(keyNameDir); err != nil {
-		return nil, err
-	}
-
-	keyNamePath := path.Join(keyNameDir, "info.toml")
-
+func NewGethKeyStoreWallet(passphrase, homePath, chainName string) (*GethKeyStoreWallet, error) {
 	// create keystore
+	keyStoreDir := path.Join(getKeyStoreDir(homePath, chainName)...)
 	store := keystore.NewKeyStore(keyStoreDir, keystore.StandardScryptN, keystore.StandardScryptP)
 
 	// load keyNameToHexAddress map
-	keyNameToHexAddress := make(map[string]string)
-	if _, err := os.Stat(keyNamePath); err != nil && !os.IsNotExist(err) {
+	keyNameInfoPath := path.Join(getKeyNameInfoPath(homePath, chainName)...)
+	b, err := internal.ReadFileIfExist(keyNameInfoPath)
+	if err != nil {
 		return nil, err
-	} else if err == nil {
-		b, err := os.ReadFile(keyNamePath)
-		if err != nil {
-			return nil, err
-		}
+	}
 
-		// unmarshal them with Config into struct
-		err = toml.Unmarshal(b, &keyNameToHexAddress)
-		if err != nil {
-			return nil, err
-		}
+	// unmarshal them with Config into struct
+	keyNameInfo := make(map[string]string)
+	err = toml.Unmarshal(b, &keyNameInfo)
+	if err != nil {
+		return nil, err
 	}
 
 	return &GethKeyStoreWallet{
-		passphrase:          passphrase,
-		store:               store,
-		keyNamePath:         keyNamePath,
-		keyNameToHexAddress: keyNameToHexAddress,
+		Passphrase:  passphrase,
+		Store:       store,
+		HomePath:    homePath,
+		KeyNameInfo: keyNameInfo,
+		ChainName:   chainName,
 	}, nil
 }
 
 // SavePrivateKey saves the private key to the keystore and returns the account and update the keyNameToHexAddress map
 func (w *GethKeyStoreWallet) SavePrivateKey(name string, privKey *ecdsa.PrivateKey) (string, error) {
-	acc, err := w.store.ImportECDSA(privKey, w.passphrase)
+	acc, err := w.Store.ImportECDSA(privKey, w.Passphrase)
 	if err != nil {
 		return "", err
 	}
 
 	addr := acc.Address.Hex()
-	w.keyNameToHexAddress[name] = addr
-	if err := w.saveKeyNameToHexAddresses(); err != nil {
+	w.KeyNameInfo[name] = addr
+	if err := w.SaveKeyNameInfo(); err != nil {
 		return "", err
 	}
 
@@ -97,7 +69,7 @@ func (w *GethKeyStoreWallet) SavePrivateKey(name string, privKey *ecdsa.PrivateK
 
 // DeletePrivateKey deletes the private key from the keystore and returns the address
 func (w *GethKeyStoreWallet) DeletePrivateKey(name string) error {
-	hexAddr, ok := w.keyNameToHexAddress[name]
+	hexAddr, ok := w.KeyNameInfo[name]
 	if !ok {
 		return fmt.Errorf("key name does not exist: %s", name)
 	}
@@ -107,25 +79,25 @@ func (w *GethKeyStoreWallet) DeletePrivateKey(name string) error {
 		return err
 	}
 
-	if err := w.store.Delete(accounts.Account{Address: addr}, w.passphrase); err != nil {
+	if err := w.Store.Delete(accounts.Account{Address: addr}, w.Passphrase); err != nil {
 		return err
 	}
 
-	delete(w.keyNameToHexAddress, name)
+	delete(w.KeyNameInfo, name)
 
-	return w.saveKeyNameToHexAddresses()
+	return w.SaveKeyNameInfo()
 }
 
 // GetAddress returns the address of the given key name
 func (w *GethKeyStoreWallet) GetAddress(name string) (string, bool) {
-	addr, ok := w.keyNameToHexAddress[name]
+	addr, ok := w.KeyNameInfo[name]
 	return addr, ok
 }
 
 // GetNames returns the list of key names
 func (w *GethKeyStoreWallet) GetNames() []string {
-	names := make([]string, 0, len(w.keyNameToHexAddress))
-	for name := range w.keyNameToHexAddress {
+	names := make([]string, 0, len(w.KeyNameInfo))
+	for name := range w.KeyNameInfo {
 		names = append(names, name)
 	}
 
@@ -134,7 +106,7 @@ func (w *GethKeyStoreWallet) GetNames() []string {
 
 // GetKey returns the private key and address of the given key name
 func (w *GethKeyStoreWallet) GetKey(name string) (*Key, error) {
-	hexAddr, ok := w.keyNameToHexAddress[name]
+	hexAddr, ok := w.KeyNameInfo[name]
 	if !ok {
 		return nil, fmt.Errorf("key name does not exist: %s", name)
 	}
@@ -144,18 +116,18 @@ func (w *GethKeyStoreWallet) GetKey(name string) (*Key, error) {
 		return nil, err
 	}
 
-	accs, err := w.store.Find(accounts.Account{Address: gethAddr})
+	accs, err := w.Store.Find(accounts.Account{Address: gethAddr})
 	if err != nil {
 		return nil, err
 	}
 
 	// need to export the key due to no direct access to the private key
-	b, err := w.store.Export(accs, w.passphrase, w.passphrase)
+	b, err := w.Store.Export(accs, w.Passphrase, w.Passphrase)
 	if err != nil {
 		return nil, err
 	}
 
-	gethKey, err := keystore.DecryptKey(b, w.passphrase)
+	gethKey, err := keystore.DecryptKey(b, w.Passphrase)
 	if err != nil {
 		return nil, err
 	}
@@ -166,14 +138,21 @@ func (w *GethKeyStoreWallet) GetKey(name string) (*Key, error) {
 	}, nil
 }
 
-// saveKeyNameToHexAddresses writes the keyNameToHexAddress map to the file
-func (w *GethKeyStoreWallet) saveKeyNameToHexAddresses() error {
-	f, err := os.Create(w.keyNamePath)
+// SaveKeyNameInfo writes the keyNameInfo map to the file
+func (w *GethKeyStoreWallet) SaveKeyNameInfo() error {
+	b, err := toml.Marshal(w.KeyNameInfo)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
 
-	encoder := toml.NewEncoder(f)
-	return encoder.Encode(w.keyNameToHexAddress)
+	return internal.Write(b, getKeyNameInfoPath(w.HomePath, w.ChainName))
+}
+
+// getKeyStoreDir returns the key store directory
+func getKeyStoreDir(homePath, chainName string) []string {
+	return []string{homePath, "keys", chainName, "priv"}
+}
+
+func getKeyNameInfoPath(homePath, chainName string) []string {
+	return []string{homePath, "keys", chainName, "info", "info.toml"}
 }
