a4c90c10a88f8c011c2b0ba555c5ef7f681f8ce2
Nattapat Iammelap <nattapat@Nattapats-MacBook-Pro.local>
2026-01-08 15:30:36 +0700

    fix: keep client connection alive

--- relayer/chains/evm/client.go
+++ relayer/chains/evm/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"sync"
 	"time"
 
 	ethereum "github.com/ethereum/go-ethereum"
@@ -45,7 +46,8 @@ type client struct {
 	Log logger.Logger
 
 	selectedEndpoint string
-	client           *ethclient.Client
+	selectedClient   *ethclient.Client
+	clients          []*ethclient.Client
 	alert            alert.Alert
 }
 
@@ -63,15 +65,40 @@ func NewClient(chainName string, cfg *EVMChainProviderConfig, log logger.Logger,
 
 // Connect connects to the EVM chain.
 func (c *client) Connect(ctx context.Context) error {
+	var wg sync.WaitGroup
+	for idx, endpoint := range c.Endpoints {
+		if c.clients[idx] != nil {
+			continue
+		}
+
+		wg.Add(1)
+		go func(endpoint string) {
+			defer wg.Done()
+			client, err := ethclient.Dial(endpoint)
+			if err != nil {
+				c.Log.Warn(
+					"Failed to connect to EVM chain",
+					"endpoint", endpoint,
+					err,
+				)
+				alert.HandleAlert(
+					c.alert,
+					alert.NewTopic(alert.ConnectSingleChainClientErrorMsg).
+						WithChainName(c.ChainName).
+						WithEndpoint(endpoint),
+					err.Error(),
+				)
+				return
+			}
+			c.clients[idx] = client
+		}(endpoint)
+	}
+
+	wg.Wait()
 	res, err := c.getClientWithMaxHeight(ctx)
 	if err != nil {
 		c.Log.Error("Failed to connect to EVM chain", err)
 		return err
-	}
-
-	// Close existing client if it exists
-	if c.client != nil {
-		c.client.Close()
 	}
 
 	// only log when new endpoint is used
@@ -80,7 +107,7 @@ func (c *client) Connect(ctx context.Context) error {
 	}
 
 	c.selectedEndpoint = res.Endpoint
-	c.client = res.Client
+	c.selectedClient = res.Client
 
 	return nil
 }
@@ -110,7 +137,7 @@ func (c *client) NonceAt(ctx context.Context, address gethcommon.Address) (uint6
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	nonce, err := c.client.NonceAt(newCtx, address, nil)
+	nonce, err := c.selectedClient.NonceAt(newCtx, address, nil)
 	if err != nil {
 		c.Log.Error(
 			"Failed to get nonce",
@@ -129,7 +156,7 @@ func (c *client) GetBlockHeight(ctx context.Context) (uint64, error) {
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	blockHeight, err := c.client.BlockNumber(newCtx)
+	blockHeight, err := c.selectedClient.BlockNumber(newCtx)
 	if err != nil {
 		c.Log.Error("Failed to get block height", "endpoint", c.selectedEndpoint, err)
 		return 0, fmt.Errorf("[EVMClient] failed to get block height: %w", err)
@@ -143,7 +170,7 @@ func (c *client) GetBlock(ctx context.Context, height *big.Int) (*gethtypes.Bloc
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	block, err := c.client.BlockByNumber(newCtx, height)
+	block, err := c.selectedClient.BlockByNumber(newCtx, height)
 	if err != nil {
 		c.Log.Error(
 			"Failed to get block by height",
@@ -163,7 +190,7 @@ func (c *client) GetTxReceipt(ctx context.Context, txHash string) (*TxReceipt, e
 	defer cancel()
 
 	var receipt *TxReceipt
-	err := c.client.Client().CallContext(newCtx, &receipt, "eth_getTransactionReceipt", txHash)
+	err := c.selectedClient.Client().CallContext(newCtx, &receipt, "eth_getTransactionReceipt", txHash)
 	if err == nil && receipt == nil {
 		// it's normal to not have receipt for pending tx
 		err = ethereum.NotFound
@@ -186,7 +213,7 @@ func (c *client) GetTxByHash(ctx context.Context, txHash string) (*gethtypes.Tra
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	tx, isPending, err := c.client.TransactionByHash(newCtx, gethcommon.HexToHash(txHash))
+	tx, isPending, err := c.selectedClient.TransactionByHash(newCtx, gethcommon.HexToHash(txHash))
 	if err != nil {
 		c.Log.Error(
 			"Failed to get tx by hash",
@@ -210,7 +237,7 @@ func (c *client) Query(ctx context.Context, gethAddr gethcommon.Address, data []
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	res, err := c.client.CallContract(newCtx, callMsg, nil)
+	res, err := c.selectedClient.CallContract(newCtx, callMsg, nil)
 	if err != nil {
 		c.Log.Error(
 			"Failed to query contract",
@@ -229,7 +256,7 @@ func (c *client) EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64,
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	gas, err := c.client.EstimateGas(newCtx, msg)
+	gas, err := c.selectedClient.EstimateGas(newCtx, msg)
 	if err != nil {
 		c.Log.Error(
 			"Failed to estimate gas",
@@ -248,7 +275,7 @@ func (c *client) EstimateGasPrice(ctx context.Context) (*big.Int, error) {
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	gasPrice, err := c.client.SuggestGasPrice(newCtx)
+	gasPrice, err := c.selectedClient.SuggestGasPrice(newCtx)
 	if err != nil {
 		c.Log.Error(
 			"Failed to estimate gas price",
@@ -267,7 +294,7 @@ func (c *client) EstimateBaseFee(ctx context.Context) (*big.Int, error) {
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	latestHeader, err := c.client.HeaderByNumber(newCtx, nil)
+	latestHeader, err := c.selectedClient.HeaderByNumber(newCtx, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -281,7 +308,7 @@ func (c *client) EstimateGasTipCap(ctx context.Context) (*big.Int, error) {
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	gasTipCap, err := c.client.SuggestGasTipCap(newCtx)
+	gasTipCap, err := c.selectedClient.SuggestGasTipCap(newCtx)
 	if err != nil {
 		c.Log.Error(
 			"Failed to estimate gas tip cap",
@@ -310,7 +337,7 @@ func (c *client) BroadcastTx(ctx context.Context, tx *gethtypes.Transaction) (st
 	newCtx, cancel := context.WithTimeout(ctx, c.ExecuteTimeout)
 	defer cancel()
 
-	if err := c.client.SendTransaction(newCtx, tx); err != nil {
+	if err := c.selectedClient.SendTransaction(newCtx, tx); err != nil {
 		c.Log.Error(
 			"Failed to broadcast tx",
 			"endpoint", c.selectedEndpoint,
@@ -328,23 +355,12 @@ func (c *client) BroadcastTx(ctx context.Context, tx *gethtypes.Transaction) (st
 func (c *client) getClientWithMaxHeight(ctx context.Context) (ClientConnectionResult, error) {
 	ch := make(chan ClientConnectionResult, len(c.Endpoints))
 
-	for _, endpoint := range c.Endpoints {
-		go func(endpoint string) {
-			client, err := ethclient.Dial(endpoint)
-			if err != nil {
-				c.Log.Warn(
-					"Failed to connect to EVM chain",
-					"endpoint", endpoint,
-					err,
-				)
+	for idx, endpoint := range c.Endpoints {
+		go func(idx int, endpoint string) {
+			client := c.clients[idx]
+
+			if client == nil {
 				ch <- ClientConnectionResult{endpoint, nil, 0}
-				alert.HandleAlert(
-					c.alert,
-					alert.NewTopic(alert.ConnectSingleChainClientErrorMsg).
-						WithChainName(c.ChainName).
-						WithEndpoint(endpoint),
-					err.Error(),
-				)
 				return
 			}
 
@@ -421,7 +437,7 @@ func (c *client) getClientWithMaxHeight(ctx context.Context) (ClientConnectionRe
 			)
 
 			ch <- ClientConnectionResult{endpoint, client, blockHeight}
-		}(endpoint)
+		}(idx, endpoint)
 	}
 
 	var result ClientConnectionResult
@@ -429,12 +445,7 @@ func (c *client) getClientWithMaxHeight(ctx context.Context) (ClientConnectionRe
 		r := <-ch
 		if r.Client != nil {
 			if r.BlockHeight > result.BlockHeight || (r.Endpoint == c.selectedEndpoint && r.BlockHeight == result.BlockHeight) {
-				if result.Client != nil {
-					result.Client.Close()
-				}
 				result = r
-			} else {
-				r.Client.Close()
 			}
 		}
 	}
@@ -455,7 +466,7 @@ func (c *client) getClientWithMaxHeight(ctx context.Context) (ClientConnectionRe
 
 // checkAndConnect checks if the client is connected to the EVM chain, if not connect it.
 func (c *client) CheckAndConnect(ctx context.Context) error {
-	if c.client != nil {
+	if c.selectedClient != nil {
 		return nil
 	}
 
@@ -467,7 +478,7 @@ func (c *client) GetBalance(ctx context.Context, gethAddr gethcommon.Address, bl
 	newCtx, cancel := context.WithTimeout(ctx, c.QueryTimeout)
 	defer cancel()
 
-	res, err := c.client.BalanceAt(newCtx, gethAddr, blockNumber)
+	res, err := c.selectedClient.BalanceAt(newCtx, gethAddr, blockNumber)
 	if err != nil {
 		c.Log.Error(
 			"Failed to query balance",
