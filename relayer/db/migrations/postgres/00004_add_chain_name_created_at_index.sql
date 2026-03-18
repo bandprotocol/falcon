bea5d6cde3956b02d63fd7eabd88e7c430f514c6
Tanut Lertwarachai <tanutlertwarachai@Tanuts-MacBook-Pro.local>
2026-03-18 18:05:05 +0700

    delete index migrate

--- relayer/db/migrations/postgres/00004_add_chain_name_created_at_index.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-CREATE INDEX idx_transactions_tunnel_id_block_timestamp ON transactions (tunnel_id, block_timestamp DESC);
-
--- +goose Down
-DROP INDEX idx_transactions_tunnel_id_block_timestamp;
